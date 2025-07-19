3c33bcdb6aab02b0d5c73eb45f0a57a074557ffc
suryansh-py <thisissuryansh07@gmail.com>
2025-07-19 22:01:00 +0530

    Little Changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -6,10 +6,10 @@ console.log(3
     +
      3) // code readability should be high
 
-console.log("Hitesh")
+console.log("Suryansh")
 
 
-let name = "hitesh"
+let name = "Suryansh"
 let age = 18
 let isLoggedIn = false
 let state;
