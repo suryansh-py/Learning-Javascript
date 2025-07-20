9873f5f6f64aefb8326f040666e37cfbdcdf012f
suryansh-py <thisissuryansh07@gmail.com>
2025-07-20 22:13:38 +0530

    datesinJs

--- 01_basics/07_datesinJs.js
+++ 01_basics/07_datesinJs.js
@@ -2,12 +2,14 @@
 // Months start from 0
 
 let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
+// console.log(myDate.toString()); // Sun Jul 20 2025 22:11:38 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString()); // Sun Jul 20 2025
+// console.log(myDate.toLocaleString()); // 20/7/2025, 10:13:02 pm
 // console.log(typeof myDate); #object
 
-// let myCreatedDate = new Date(2023, 0, 23)
+let myCreatedDate = new Date(2025, 6, 20)
+console.log(myCreatedDate.toDateString()); // Sun Jul 20 2025
+
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3)
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
