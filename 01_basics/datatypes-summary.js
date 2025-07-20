242d6a96353a6d1ba98e34b0647b605e015638b6
suryansh-py <thisissuryansh07@gmail.com>
2025-07-20 22:02:50 +0530

    Revision

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -25,7 +25,7 @@ console.log(typeof outsideTemp); //object
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
+    name: "suryansh",
     age: 22,
 }
 
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -14,4 +14,12 @@ result = prompt(title, //default);
 //default
 //An optional second parameter, the initial value for the input field.
 //The square brackets in syntax [...] The square brackets around default in the syntax above denote that the parameter is optional, not required
-    
+
+//confirm
+result = confirm(question);
+//The function confirm shows a modal window with a question and two buttons: OK and Cancel.
+//The result is true if OK is pressed and false otherwise.
+//For example:
+
+let isBoss = confirm("Are you the boss?");
+alert( isBoss ); // true if OK is pressed
